eafcf83c6bc6d72b152a6846c642772db52ed051
nngig <ngigi.francis5@gmail.com>
2024-02-17 08:59:11 +0300

    done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -4,9 +4,16 @@ function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
+      <button></button>
       {/* render <button> elements for each category here */}
     </div>
   );
 }
 
 export default CategoryFilter;
+Any time you want to make a change (adding, updating or deleting an item) to an array that's being stored in state, you first need to make a copy of the array using the spread operator.
+Group of answer choices
+
+True
+
+False
